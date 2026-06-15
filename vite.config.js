028443ad13726f6e2fad0718fceba5fd66ffb2ab
DmitroOmelianenko <dmitroomelianenko@gmail.com>
2026-06-15 21:29:37 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/practies/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
